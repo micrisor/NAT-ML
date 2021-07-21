9cd860f2f23fcf5e7a7df7778684e4aa3f629459
micrisor <16051711+micrisor@users.noreply.github.com>
2021-07-21 17:29:15 +0100

    Added generic paths to schedulers

--- training_code/schedule.py
+++ training_code/schedule.py
@@ -5,12 +5,12 @@ def submission(feats, her2word, her2num, rcut, rs, stamp):
 #SBATCH --mem=4G
 #SBATCH --time=12:00:00
 #SBATCH --mail-type=FAIL
-#SBATCH --mail-user=mireia.crispinortuzar@cruk.cam.ac.uk
+#SBATCH --mail-user=email@email.com
 #SBATCH -p general
 #SBATCH -o logs/slurm_{stamp}_{feats}_her2{her2word}_r{rcut}_rs{rs}.out
 #SBATCH -e logs/slurm_{stamp}_{feats}_her2{her2word}_r{rcut}_rs{rs}.err
 
-singularity exec -H /home/crispi01/projects/breast/response/MolecularData_Classification:/home -B /scratcha/fmlab/crispi01/projects/breast/MolecularData_Classification/results_{stamp}_r{rcut}_her2{her2word}_rs{rs}:/data pythonenv.img python run_models.py {feats} {her2num} {rcut} {rs}
+singularity exec -H /the/directory/with/the/files:/home -B /the/output/directory/results_{stamp}_r{rcut}_her2{her2word}_rs{rs}:/data pythonenv.img python run_models.py {feats} {her2num} {rcut} {rs}
 '''.format(feats=feats, her2num=her2num, her2word=her2word, stamp=stamp, rcut=rcut, rs=rs)
     return script
 
@@ -43,9 +43,9 @@ def main():
         her2_i_num = her2_dic[her2_i]
         print(her2_i_num)
         outdir = 'results_{}_r{}_her2{}_rs{}'.format(stamp, rcut_i, her2_i, rs_i)
-        if not os.path.exists('/scratcha/fmlab/crispi01/projects/breast/MolecularData_Classification/{}'.format(outdir)):
-            os.makedirs('/scratcha/fmlab/crispi01/projects/breast/MolecularData_Classification/{}'.format(outdir))
-            print('Making: /scratcha/fmlab/crispi01/projects/breast/MolecularData_Classification/{}'.format(outdir))
+        if not os.path.exists('/the/output/directory/{}'.format(outdir)):
+            os.makedirs('/the/output/directory/{}'.format(outdir))
+            print('Making: /the/output/directory/{}'.format(outdir))
 
         script = submission(feat_i, her2_i, her2_i_num, rcut_i, rs_i, stamp)
 
